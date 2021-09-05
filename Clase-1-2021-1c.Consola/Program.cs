ceedadae80033975364f7087b40a5ca576e4978d
Demian <fioranidemian@gmail.com>
2021-09-05 01:58:51 -0300

    Se resuelve el ejercicio

--- Clase-1-2021-1c.Consola/Program.cs
+++ Clase-1-2021-1c.Consola/Program.cs
@@ -7,7 +7,8 @@ namespace Clase_1_2021_1c.Consola
     {
         static void Main(string[] args)
         {
-            EjercicioBola8Magica();
+            EjercicioIMC();
+            //EjercicioBola8Magica();
             //EjericicioCalculadora();
         }
 
@@ -38,6 +39,31 @@ namespace Clase_1_2021_1c.Consola
 
             //Console.WriteLine("La suma es: " + Calculadora.Sumar(strNum1, strNum2));
             Console.WriteLine($"La suma es: {Calculadora.Sumar(strNum1, strNum2)}");
+        }
+
+        private static void EjercicioIMC()
+        {
+            string pregunta;
+            Console.WriteLine("Si quiere salir ingrese 0, para calcular su IMC ingrese cualquier otro valor");
+            pregunta = Console.ReadLine();
+
+            while (pregunta != "0")
+            {
+                Console.WriteLine("Ingrese su altura");
+                string altura = Console.ReadLine();
+                Console.WriteLine("Ingrese su peso");
+                string peso = Console.ReadLine();
+
+                string detalles;
+                double imc = IMC.calcularIMC(peso, altura, out detalles);
+
+                Console.WriteLine($"Su IMC es: {imc}");
+                Console.WriteLine(detalles);
+
+
+                Console.WriteLine("Si quiere salir ingrese 0, para calcular su IMC ingrese cualquier otro valor");
+                pregunta = Console.ReadLine();
+            }
         }
     }
 }
---
+++ PW3-2021-2c-Clase-1.Logica/IMC.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PW3_2021_2c_Clase_1.Logica
+{
+    public class IMC
+    {
+        public static double calcularIMC(string peso, string altura, out string detalles)
+        {
+            if (!double.TryParse(peso, out double doublePeso))
+            {
+                throw new ArgumentException("El valor ingresado no es un nro entero valido.");
+            }
+
+            if (!double.TryParse(altura, out double doubleAltura))
+            {
+                throw new ArgumentException("El valor ingresado no es un nro entero valido.");
+            }
+
+            double resultado = doublePeso / (doubleAltura * doubleAltura);
+
+
+            if( resultado < 18.50){
+                detalles = "Bajo peso - ";
+                if(resultado < 16)
+                {
+                    detalles += "Delgadez severa (<16,00)";
+                }
+                else if (resultado > 17)
+                {
+                    detalles += "Delgadez leve (17,00 - 18,49)";
+                }
+                else
+                {
+                    detalles += "Delgadez moderada (16,00 - 16,99)";
+                }
+            }
+            else if(resultado >= 18.5 && resultado < 25)
+            { 
+                detalles = "Normal (18,5 - 24,99)";
+            }
+            else if (resultado >= 30)
+            {
+                detalles = "Obesidad - ";
+                if (resultado >= 40)
+                {
+                    detalles += "Obesidad mórbida (>=40,00)";
+                }
+                else if (resultado > 17)
+                {
+                    detalles += "Obesidad leve (30,00 - 34,99)";
+                }
+                else
+                {
+                    detalles += "Obesidad media (35,00 - 39,99)";
+                }
+            }
+            else
+            {
+                detalles = "Sobrepeso - Preobesidad (25,00 - 29,99)";
+            }
+
+            return resultado;
+        }
+    }
+}
